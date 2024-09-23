22cc25fa1c5db85255891aa394b524382c7f9fa4
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2024-09-23 14:35:36 +0200

    feat: add primelocale translation loading

--- src/app/bookmarks/bookmarks.module.ts
+++ src/app/bookmarks/bookmarks.module.ts
@@ -1,30 +1,23 @@
 import { CommonModule } from '@angular/common'
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
+import { NgModule } from '@angular/core'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
 import { LetDirective } from '@ngrx/component'
 import { EffectsModule } from '@ngrx/effects'
 import { BookmarksCreateUpdateComponent } from './pages/bookmarks-search/dialogs/bookmarks-create-update/bookmarks-create-update.component'
-import {
-  PortalMissingTranslationHandler,
-  createTranslateLoader,
-  providePortalDialogService,
-  PortalCoreModule
-} from '@onecx/portal-integration-angular'
+import { providePortalDialogService, PortalCoreModule } from '@onecx/portal-integration-angular'
 import { StoreModule } from '@ngrx/store'
-import { MissingTranslationHandler, TranslateLoader, TranslateModule } from '@ngx-translate/core'
-import { AppStateService, addInitializeModuleGuard } from '@onecx/angular-integration-interface'
+import { TranslateModule } from '@ngx-translate/core'
+import { addInitializeModuleGuard } from '@onecx/angular-integration-interface'
 import { SharedModule } from '../shared/shared.module'
 import { bookmarksFeature } from './bookmarks.reducers'
 import { routes } from './bookmarks.routes'
 import { BookmarksSearchComponent } from './pages/bookmarks-search/bookmarks-search.component'
 import { BookmarksSearchEffects } from './pages/bookmarks-search/bookmarks-search.effects'
-import { HttpClient } from '@angular/common/http'
 
 @NgModule({
   providers: [providePortalDialogService()],
   declarations: [BookmarksCreateUpdateComponent, BookmarksSearchComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
     SharedModule,
@@ -35,19 +28,7 @@ import { HttpClient } from '@angular/common/http'
     ReactiveFormsModule,
     StoreModule.forFeature(bookmarksFeature),
     EffectsModule.forFeature([BookmarksSearchEffects]),
-    TranslateModule.forRoot({
-      extend: true,
-      isolate: false,
-      loader: {
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [HttpClient, AppStateService]
-      },
-      missingTranslationHandler: {
-        provide: MissingTranslationHandler,
-        useClass: PortalMissingTranslationHandler
-      }
-    })
+    TranslateModule
   ]
 })
 export class BookmarksModule {}
