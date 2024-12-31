90c2013f75cd28fb1cb3a001f315c3cfb8e2ee34
Henry Taeschner <henry.taeschner@capgemini.com>
2024-12-31 11:55:43 +0100

    fix: layout result table

--- src/app/bookmarks/pages/bookmarks-search/bookmarks-search.columns.ts
+++ src/app/bookmarks/pages/bookmarks-search/bookmarks-search.columns.ts
@@ -5,6 +5,7 @@ export const bookmarksSearchColumns: DataTableColumn[] = [
   { id: 'displayName', columnType: ColumnType.STRING, nameKey: 'BOOKMARK.DISPLAY_NAME', sortable: true },
   { id: 'position', columnType: ColumnType.NUMBER, nameKey: 'BOOKMARK.POSITION', sortable: true },
   { id: 'workspace', columnType: ColumnType.STRING, nameKey: 'BOOKMARK.WORKSPACE', sortable: true },
-  { id: 'productName', columnType: ColumnType.STRING, nameKey: 'BOOKMARK.PRODUCT_NAME', sortable: true }
+  { id: 'productName', columnType: ColumnType.STRING, nameKey: 'BOOKMARK.PRODUCT_NAME', sortable: true },
+  { id: 'creationDate', columnType: ColumnType.DATE, nameKey: 'INTERNAL.CREATION_DATE', sortable: true }
   //{ id: 'appId', columnType: ColumnType.STRING, nameKey: 'BOOKMARK.APP_ID', sortable: true }
 ]
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,5 +1,5 @@
 import { bootstrapModule } from '@onecx/angular-webcomponents'
 import { environment } from 'src/environments/environment'
-import { OneCXBookmarkModule } from './app/onecx-bookmark-ui.remote.module'
+import { OneCXBookmarkModule } from './app/onecx-bookmark.remote.module'
 
 bootstrapModule(OneCXBookmarkModule, 'microfrontend', environment.production)
