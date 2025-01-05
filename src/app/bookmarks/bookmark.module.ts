b86e8609cd36deb6b12a3bfebe9dd1060c2c7e98
Henry Taeschner <henry.taeschner@capgemini.com>
2025-01-05 11:16:06 +0100

    fix: simplify component structure, added error tailor

--- src/app/bookmarks/bookmark.module.ts
+++ src/app/bookmarks/bookmark.module.ts
@@ -16,8 +16,8 @@ import { routes } from './bookmark.routes'
 
 import { BookmarkSearchEffects } from './pages/bookmark-search/bookmark-search.effects'
 import { BookmarkSearchComponent } from './pages/bookmark-search/bookmark-search.component'
-import { BookmarkDeleteComponent } from './pages/bookmark-search/dialogs/bookmark-delete/bookmark-delete.component'
-import { BookmarkDetailComponent } from './pages/bookmark-search/dialogs/bookmark-detail/bookmark-detail.component'
+import { BookmarkDeleteComponent } from './pages/bookmark-search/bookmark-delete/bookmark-delete.component'
+import { BookmarkDetailComponent } from './pages/bookmark-search/bookmark-detail/bookmark-detail.component'
 import { BookmarkImageComponent } from './pages/bookmark-search/product-image/bookmark-image.component'
 
 @NgModule({
--- src/app/remotes/manage-bookmark/bookmark-create-update/bookmark-create-update.component.ts
+++ src/app/remotes/manage-bookmark/bookmark-create-update/bookmark-create-update.component.ts
@@ -1,6 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
-import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { FormsModule, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { TranslateModule, TranslateService } from '@ngx-translate/core'
+import { MessagesModule } from 'primeng/messages'
+import { SharedModule } from 'primeng/api'
+import { TooltipModule } from 'primeng/tooltip'
 import { map } from 'rxjs'
+import { provideErrorTailorConfig, errorTailorImports } from '@ngneat/error-tailor'
 
 import {
   DialogButtonClicked,
@@ -14,8 +19,42 @@ import { Bookmark, BookmarkScopeEnum } from 'src/app/shared/generated'
 import { BookmarkCreateUpdateViewModel } from './bookmark-create-update.viewmodel'
 
 @Component({
+  standalone: true,
+  imports: [
+    errorTailorImports,
+    FormsModule,
+    MessagesModule,
+    ReactiveFormsModule,
+    SharedModule,
+    TooltipModule,
+    TranslateModule
+  ],
   selector: 'app-bookmark-create-update',
-  templateUrl: './bookmark-create-update.component.html'
+  templateUrl: './bookmark-create-update.component.html',
+  providers: [
+    provideErrorTailorConfig({
+      controlErrorsOn: { async: true, blur: true, change: true },
+      errors: {
+        useFactory: (i18n: TranslateService) => {
+          return {
+            required: () => i18n.instant('VALIDATION.ERRORS.EMPTY_REQUIRED_FIELD'),
+            maxlength: ({ requiredLength }) =>
+              i18n.instant('VALIDATION.ERRORS.MAXIMUM_LENGTH').replace('{{chars}}', requiredLength),
+            minlength: ({ requiredLength }) =>
+              i18n.instant('VALIDATION.ERRORS.MINIMUM_LENGTH').replace('{{chars}}', requiredLength),
+            pattern: () => i18n.instant('VALIDATION.ERRORS.PATTERN_ERROR')
+          }
+        },
+        deps: [TranslateService]
+      },
+      //this is required because primeng calendar wraps things in an ugly way
+      blurPredicate: (element: Element) => {
+        return ['INPUT', 'TEXTAREA', 'SELECT', 'CUSTOM-DATE', 'P-CALENDAR', 'P-DROPDOWN'].some(
+          (selector) => element.tagName === selector
+        )
+      }
+    })
+  ]
 })
 export class BookmarkCreateUpdateComponent
   implements
@@ -34,9 +73,12 @@ export class BookmarkCreateUpdateComponent
   public dialogResult: Bookmark | undefined = undefined
   public isPublicBookmark = false
 
-  constructor(private readonly userService: UserService) {
+  constructor(
+    private readonly userService: UserService,
+    private readonly translate: TranslateService
+  ) {
     this.formGroup = new FormGroup({
-      displayName: new FormControl(null, [Validators.maxLength(255), Validators.required])
+      displayName: new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(255)])
     })
     this.formGroup.statusChanges
       .pipe(
